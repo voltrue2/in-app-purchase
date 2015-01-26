238cdea0d0c6773597ebf297c9ce36c6bf1983f6
voltrue2 <voltrue2@gmail.com>
2015-01-26 10:24:51 +0900

    add google public key as string support

--- test/google.js
+++ test/google.js
@@ -60,7 +60,7 @@ describe('iap', function () {
 
 	/***************************
 	* With Public Key From ENV *
-	****************************/
+	***************************/
 		
 	it('Can validate google in-app-purchase with public key from ENV variable', function (done) {
 	
@@ -91,6 +91,50 @@ describe('iap', function () {
 						}
 						console.log(data);
 						exec('unset GOOGLE_IAB_PUBLICKEY_SANDBOX', done);
+					});
+				});
+			});
+		});
+	
+	});
+
+	/****************************
+	* With Public Key As String *
+	****************************/
+		
+	it('Can validate google in-app-purchase with public key as string', function (done) {
+	
+		var exec = require('child_process').exec;	
+		var path = process.argv[process.argv.length - 2].replace('--path=', '');
+		var pkPath = process.argv[process.argv.length - 1].replace('--pk=', '');
+
+		var iap = require('../');
+		iap.reset();
+		fs.readFile(pkPath + 'iap-live', 'utf-8', function (error, pkeyValue) {
+			assert.equal(error, undefined);
+			iap.config({
+				googlePublicKeyStrLive: pkeyValue
+			});
+			fs.readFile(pkPath + 'iap-sandbox', 'utf8', function (error, pk) {
+				assert.equal(error, undefined);
+				// now test
+				iap.setup(function (error) {
+					assert.equal(error, undefined);
+					fs.readFile(path, function (error, data) {
+						assert.equal(error, undefined);
+						var receipt = data.toString();
+						iap.validate(iap.GOOGLE, JSON.parse(receipt), function (error, response) {
+							assert.equal(error, undefined);
+							assert.equal(iap.isValidated(response), true);
+							var data = iap.getPurchaseData(response);
+							for (var i = 0, len = data.length; i < len; i++) {
+								assert(data[i].productId);
+								assert(data[i].purchaseDate);
+								assert(data[i].quantity);
+							}
+							console.log(data);
+							exec('unset GOOGLE_IAB_PUBLICKEY_SANDBOX', done);
+						});
 					});
 				});
 			});

f33098dc9941695821aea0e2c51f6e6e33f48c6a
voltrue2 <voltrue2@gmail.com>
2015-04-06 20:23:32 +0900

    fixed the way google.js handles googlePublicKeyStrLive and googlePublicKeySandbox properties this should fix the issue related to this set up

--- test/google.js
+++ test/google.js
@@ -58,6 +58,94 @@ describe('iap', function () {
 	
 	});
 
+	/****************************
+	* With Public Key As String *
+	****************************/
+		
+	it('Can validate google in-app-purchase with public key as string "publicKeyStrLive"', function (done) {
+	
+		var exec = require('child_process').exec;	
+		var path = process.argv[process.argv.length - 2].replace('--path=', '');
+		var pkPath = process.argv[process.argv.length - 1].replace('--pk=', '');
+
+		var iap = require('../');
+		iap.reset();
+		fs.readFile(pkPath + 'iap-live', 'utf-8', function (error, pkeyValue) {
+			assert.equal(error, undefined);
+			iap.config({
+				googlePublicKeyPath: null,
+				publicKeyStrLive: pkeyValue.replace(/(\r\n|\n|\r)/gm, ''),
+				publicKeyStrSandbox: pkeyValue.replace(/(\r\n|\n|\r)/gm, '')
+			});
+			fs.readFile(pkPath + 'iap-sandbox', 'utf8', function (error, pk) {
+				assert.equal(error, undefined);
+				// now test
+				iap.setup(function (error) {
+					assert.equal(error, undefined);
+					fs.readFile(path, function (error, data) {
+						assert.equal(error, undefined);
+						var receipt = data.toString();
+						iap.validate(iap.GOOGLE, JSON.parse(receipt), function (error, response) {
+							assert.equal(error, undefined);
+							assert.equal(iap.isValidated(response), true);
+							var data = iap.getPurchaseData(response);
+							for (var i = 0, len = data.length; i < len; i++) {
+								assert(data[i].productId);
+								assert(data[i].purchaseDate);
+								assert(data[i].quantity);
+							}
+							console.log(data);
+							exec('unset GOOGLE_IAB_PUBLICKEY_SANDBOX', done);
+						});
+					});
+				});
+			});
+		});
+	
+	});
+		
+	it('Can validate google in-app-purchase with public key as string "googlePublicKeyStrLive"', function (done) {
+	
+		var exec = require('child_process').exec;	
+		var path = process.argv[process.argv.length - 2].replace('--path=', '');
+		var pkPath = process.argv[process.argv.length - 1].replace('--pk=', '');
+
+		var iap = require('../');
+		iap.reset();
+		fs.readFile(pkPath + 'iap-live', 'utf-8', function (error, pkeyValue) {
+			assert.equal(error, undefined);
+			iap.config({
+				googlePublicKeyPath: null,
+				googlePublicKeyStrLive: pkeyValue.replace(/(\r\n|\n|\r)/gm, ''),
+				googlePublicKeyStrSandbox: pkeyValue.replace(/(\r\n|\n|\r)/gm, '')
+			});
+			fs.readFile(pkPath + 'iap-sandbox', 'utf8', function (error, pk) {
+				assert.equal(error, undefined);
+				// now test
+				iap.setup(function (error) {
+					assert.equal(error, undefined);
+					fs.readFile(path, function (error, data) {
+						assert.equal(error, undefined);
+						var receipt = data.toString();
+						iap.validate(iap.GOOGLE, JSON.parse(receipt), function (error, response) {
+							assert.equal(error, undefined);
+							assert.equal(iap.isValidated(response), true);
+							var data = iap.getPurchaseData(response);
+							for (var i = 0, len = data.length; i < len; i++) {
+								assert(data[i].productId);
+								assert(data[i].purchaseDate);
+								assert(data[i].quantity);
+							}
+							console.log(data);
+							exec('unset GOOGLE_IAB_PUBLICKEY_SANDBOX', done);
+						});
+					});
+				});
+			});
+		});
+	
+	});
+
 	/***************************
 	* With Public Key From ENV *
 	***************************/
@@ -92,90 +180,6 @@ describe('iap', function () {
 						}
 						console.log(data);
 						exec('unset GOOGLE_IAB_PUBLICKEY_SANDBOX', done);
-					});
-				});
-			});
-		});
-	
-	});
-
-	/****************************
-	* With Public Key As String *
-	****************************/
-		
-	it('Can validate google in-app-purchase with public key as string', function (done) {
-	
-		var exec = require('child_process').exec;	
-		var path = process.argv[process.argv.length - 2].replace('--path=', '');
-		var pkPath = process.argv[process.argv.length - 1].replace('--pk=', '');
-
-		var iap = require('../');
-		iap.reset();
-		fs.readFile(pkPath + 'iap-live', 'utf-8', function (error, pkeyValue) {
-			assert.equal(error, undefined);
-			iap.config({
-				publicKeyStrLive: pkeyValue
-			});
-			fs.readFile(pkPath + 'iap-sandbox', 'utf8', function (error, pk) {
-				assert.equal(error, undefined);
-				// now test
-				iap.setup(function (error) {
-					assert.equal(error, undefined);
-					fs.readFile(path, function (error, data) {
-						assert.equal(error, undefined);
-						var receipt = data.toString();
-						iap.validate(iap.GOOGLE, JSON.parse(receipt), function (error, response) {
-							assert.equal(error, undefined);
-							assert.equal(iap.isValidated(response), true);
-							var data = iap.getPurchaseData(response);
-							for (var i = 0, len = data.length; i < len; i++) {
-								assert(data[i].productId);
-								assert(data[i].purchaseDate);
-								assert(data[i].quantity);
-							}
-							console.log(data);
-							exec('unset GOOGLE_IAB_PUBLICKEY_SANDBOX', done);
-						});
-					});
-				});
-			});
-		});
-	
-	});
-		
-	it('Can validate google in-app-purchase with public key as string', function (done) {
-	
-		var exec = require('child_process').exec;	
-		var path = process.argv[process.argv.length - 2].replace('--path=', '');
-		var pkPath = process.argv[process.argv.length - 1].replace('--pk=', '');
-
-		var iap = require('../');
-		iap.reset();
-		fs.readFile(pkPath + 'iap-live', 'utf-8', function (error, pkeyValue) {
-			assert.equal(error, undefined);
-			iap.config({
-				googlePublicKeyStrLive: pkeyValue
-			});
-			fs.readFile(pkPath + 'iap-sandbox', 'utf8', function (error, pk) {
-				assert.equal(error, undefined);
-				// now test
-				iap.setup(function (error) {
-					assert.equal(error, undefined);
-					fs.readFile(path, function (error, data) {
-						assert.equal(error, undefined);
-						var receipt = data.toString();
-						iap.validate(iap.GOOGLE, JSON.parse(receipt), function (error, response) {
-							assert.equal(error, undefined);
-							assert.equal(iap.isValidated(response), true);
-							var data = iap.getPurchaseData(response);
-							for (var i = 0, len = data.length; i < len; i++) {
-								assert(data[i].productId);
-								assert(data[i].purchaseDate);
-								assert(data[i].quantity);
-							}
-							console.log(data);
-							exec('unset GOOGLE_IAB_PUBLICKEY_SANDBOX', done);
-						});
 					});
 				});
 			});

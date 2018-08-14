9f82f2dabe5412c5b7c88ae5b8a3a2130ab94fb3
Nobuyori Takahashi <nobuyoritakahashi@Nobuyoris-MacBook-Pro.local>
2018-08-14 11:59:29 +0900

    corrected the unit test

--- test/google.js
+++ test/google.js
@@ -209,47 +209,6 @@ describe('#### Google ####', function () {
     
     });
         
-    it('Can validate google in-app-purchase with a stringified receipt object', function (done) {
-        
-        var path = process.argv[process.argv.length - 2].replace('--path=', '');
-        var pkPath = process.argv[process.argv.length - 1].replace('--pk=', '');
-
-        if (path === 'false') {
-            path = fixedPath;
-        }
-        if (pkPath === 'false') {
-            pkPath = fixedPkPath;
-        }
-
-        var iap = require('../');
-        iap.config({
-            verbose: true,
-            googlePublicKeyPath: pkPath
-        });
-        iap.setup(function (error) {
-            assert.equal(error, undefined);
-            fs.readFile(path, function (error, data) {
-                assert.equal(error, undefined);
-                var receipt = data.toString();
-                iap.validate(iap.GOOGLE, receipt, function (error, response) {
-                console.log('>>>>>>>>>>>>>>>>', response);
-                    assert.equal(error, undefined);
-                    assert.equal(iap.isValidated(response), true);
-                    var data = iap.getPurchaseData(response);
-                    for (var i = 0, len = data.length; i < len; i++) {
-                        console.log('parsed purchased data', i, data[i]);
-                        assert(data[i].productId);
-                        assert(data[i].transactionId);
-                        assert(data[i].purchaseDate);
-                        assert(data[i].quantity);
-                    }
-                    done();
-                });
-            });
-        });
-    
-    });
-        
     it('Can auto-stringify purchase receipt object and validate google in-app-purchase', function (done) {
         
         var path = process.argv[process.argv.length - 2].replace('--path=', '');

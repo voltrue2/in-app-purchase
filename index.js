cb0dd0e04b6efdfd1db7c1ab445fe678b1ed875e
Quok Games <quokgames@gmail.com>
2026-01-26 18:10:54 +0300

    feat: Embed Apple Root CA G3 certificate for Apple StoreKit 2 signed data verification and update a console log message.

--- index.js
+++ index.js
@@ -125,12 +125,11 @@ module.exports.getService = function (receipt) {
 };
 
 module.exports.validate = function (service, receipt, cb) {
-    console.log('new validate', service, receipt, cb);
+    console.log('test update', service, receipt, cb);
     if (receipt === undefined && cb === undefined) {
         // we are given 1 argument as: const promise = .validate(receipt)
         receipt = service;
         service = module.exports.getService(receipt);
-        console.log('new validate 1', service, receipt, cb);
     }
     if (cb === undefined && typeof receipt === 'function') {
         // we are given 2 arguments as: .validate(receipt, cb)
